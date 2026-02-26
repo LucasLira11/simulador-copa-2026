4bd8098eba3d41ef19a0991838a04772131324d5
Lucas Lira <lucasponteslira@gmail.com>
2026-02-26 14:17:55 -0300

    transformando em aplicativo via json

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -12,9 +12,21 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+
+
+export const viewport: Viewport = {
+  themeColor: "#09090b",
+};
+
 export const metadata: Metadata = {
   title: "Simulador Copa 2026",
   description: "Monte seu chaveamento oficial e simule quem vai levantar a taça da Copa do Mundo!",
+  manifest: "/manifest.json", // <-- AVISA PRO NEXT.JS QUE É UM APP
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "Copa 2026",
+  },
 };
 
 export default function RootLayout({
